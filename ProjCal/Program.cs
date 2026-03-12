21959cdde828218d753d65a43783ef17babdb937
LuizBotta <luizbottaa@gmail.com>
2026-03-12 11:57:11 -0300

    Add arquivos funções

---
+++ ProjCal/Program.cs
@@ -0,0 +1,26 @@
+﻿int Soma(int n1, int n2)
+{
+    return n1 + n2;
+}
+
+int Sub(int n1, int n2){
+    return n1 - n2;
+}
+
+double Raiz(double n3){
+    double raiz = Math.Sqrt(n3);
+    return raiz;
+}
+
+Console.WriteLine("Digite um numero: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite um numero: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite um numero: ");
+double n3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine(Soma(n1, n2));
+Console.WriteLine(Sub(n1, n2));
+Console.WriteLine(Raiz(n3));
